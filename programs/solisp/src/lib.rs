b8153dc613b3c2cf75abc075c000b388fb8ca2a8
Mike Frager <mfrager@gmail.com>
2021-11-16 18:39:55 -0800

    Implement cmd with perfect hashing

--- programs/solisp/src/lib.rs
+++ programs/solisp/src/lib.rs
@@ -1,17 +1,36 @@
 use std::{rc::Rc, cell::RefCell};
-use rust_lisp::{parse,eval,default_env,utils::require_int_parameter,model::{Value,RuntimeError}};
-use anchor_spl::token::{ self, Transfer };
+use std::convert::TryFrom;
+use rust_lisp::{parse,eval,default_env,utils::{require_string_parameter, require_int_parameter},model::{Value,RuntimeError}};
+use phf::phf_map;
+use anchor_spl::token::{ self, accessor::amount, Transfer };
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
+#[derive(Clone)]
+pub enum Cmd {
+    TokenBalance,
+    TokenTransfer,
+}
+
+static CMDS: phf::Map<&'static str, Cmd> = phf_map! {
+    "token_balance" => Cmd::TokenBalance,
+    "token_transfer" => Cmd::TokenTransfer,
+};
+
+fn token_balance<'info>(
+    token: &AccountInfo<'info>,
+) -> Result<u64, ProgramError> {
+    amount(token)
+}
+ 
 fn token_transfer<'info>(
     prog: &AccountInfo<'info>,
     from: &AccountInfo<'info>,
     to: &AccountInfo<'info>,
     auth: &AccountInfo<'info>,
-    amt: i32,
-    dcm: i32,
+    amt: u64,
+    dcm: u32,
 ) -> ProgramResult {
     let cpi_accounts = Transfer {
         from: from.clone(),
@@ -19,9 +38,7 @@ fn token_transfer<'info>(
         authority: auth.clone(),
     };
     let cpi_ctx = CpiContext::new(prog.clone(), cpi_accounts);
-    let mut final_amount: u64 = amt as u64;
-    let decimals = dcm as u32;
-    final_amount = final_amount * 10u64.pow(decimals);
+    let final_amount = amt * 10u64.pow(dcm);
     token::transfer(cpi_ctx, final_amount)?;
     Ok(())
 }
@@ -29,44 +46,60 @@ fn token_transfer<'info>(
 #[program]
 pub mod solisp {
     use super::*;
-    pub fn run<'info>(ctx: Context<'_, '_, '_, 'info, Runtime<'info>>, script: String) -> ProgramResult {
+    pub fn run<'info>(ctx: Context<'_, '_, '_, 'info, Runtime>, script: String) -> ProgramResult {
         let env = Rc::new(RefCell::new(default_env()));
         let mut accts = Vec::<AccountInfo>::new();
-        accts.push(ctx.accounts.token_program.clone());
-        accts.push(ctx.accounts.user.clone());
         for i in ctx.remaining_accounts {
             accts.push(i.clone());
         }
         env.borrow_mut().entries.insert(
-            String::from("transfer"),
+            String::from("cmd"),
             Value::NativeFunc(|_env, args, accts| {
-                let from_index = require_int_parameter("nth", args, 0)?;
-                let to_index = require_int_parameter("nth", args, 1)?;
-                let amt = require_int_parameter("nth", args, 2)?;
-                let dcm = require_int_parameter("nth", args, 3)?;
-                let from: &AccountInfo = accts.get(from_index as usize + 2).unwrap();
-                let to: &AccountInfo = accts.get(to_index as usize + 2).unwrap();
-                let res = token_transfer(accts.get(0).unwrap(), from, to, accts.get(1).unwrap(), amt, dcm);
-                if res.is_err() {
+                let cmdname = require_string_parameter("cmd", args, 0)?;
+                let cmd = CMDS.get(cmdname).cloned();
+                if cmd.is_none() {
                     return Err(RuntimeError {
-                        msg: "Transfer failed".to_string(),
+                        msg: format!("Command not found: {}", cmdname),
                     });
                 }
-                return Ok(Value::NIL);
+                match cmd.unwrap() {
+                    Cmd::TokenBalance => {
+                        let tkn_index = require_int_parameter("cmd", args, 1)?;
+                        let tkn: &AccountInfo = accts.get(tkn_index as usize).unwrap();
+                        let res = token_balance(tkn);
+                        if res.is_err() {
+                            return Err(RuntimeError { msg: format!("Command failed: {}", cmdname) })
+                        }
+                        return Ok(Value::Int(res.unwrap() as i128));
+                    },
+                    Cmd::TokenTransfer => {
+                        let prg_index = require_int_parameter("cmd", args, 1)?;
+                        let from_index = require_int_parameter("cmd", args, 2)?;
+                        let to_index = require_int_parameter("cmd", args, 3)?;
+                        let auth_index = require_int_parameter("cmd", args, 4)?;
+                        let amt = require_int_parameter("cmd", args, 5)?;
+                        let dcm = require_int_parameter("cmd", args, 6)?;
+                        let prg: &AccountInfo = accts.get(prg_index as usize).unwrap();
+                        let from: &AccountInfo = accts.get(from_index as usize).unwrap();
+                        let to: &AccountInfo = accts.get(to_index as usize).unwrap();
+                        let auth: &AccountInfo = accts.get(auth_index as usize).unwrap();
+                        let res = token_transfer(prg, from, to, auth, u64::try_from(amt).unwrap(), u32::try_from(dcm).unwrap());
+                        if res.is_err() {
+                            return Err(RuntimeError { msg: format!("Command failed: {}", cmdname) })
+                        }
+                    },
+                };
+
+                return Ok(Value::True);
             })
         );
         let mut ast_iter = parse(script.as_str());
-        let first_expression = ast_iter.next().unwrap().unwrap();
-        let evaluation_result = eval(env.clone(), &first_expression, &accts).unwrap();
-        msg!("Atellix: Result: {}", &evaluation_result);
+        let expr = ast_iter.next().unwrap().unwrap();
+        let result = eval(env.clone(), &expr, &accts).unwrap();
+        msg!("Atellix: Result: {}", &result);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Runtime<'info> {
-    #[account(address = token::ID)]
-    pub token_program: AccountInfo<'info>,
-    #[account(signer)]
-    pub user: AccountInfo<'info>,
-}
+pub struct Runtime {}
--- tests/solisp.js
+++ tests/solisp.js
@@ -9,18 +9,25 @@ describe('solisp', () => {
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.Solisp;
-    const tx = await program.rpc.run('(transfer 0 1 2 4)',
+    const tx = await program.rpc.run('(begin (let ((foo 12) (bar (+ 4 3)) (blah "stuff")) (print foo) (print bar) (print blah) (* foo bar)))');
+/*    const tx = await program.rpc.run('(let ((bal (cmd "token_balance" 0))) (/ (* bal 5) 1000))',
         {
-            accounts: {
-                tokenProgram: new anchor.web3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'),
-                user: provider.wallet.publicKey,
-            },
             remainingAccounts: [
-                { pubkey: new anchor.web3.PublicKey('Ha2s5qVfW8HFpTwkEfuJm6NLJSSoBgLSKTTkMaCoVKpS'), isWritable: true, isSigner: false },
-                { pubkey: new anchor.web3.PublicKey('AC5aWi4GXaCMfXPKTfASPCEuvNpQs3KgccTYqywqR8HM'), isWritable: true, isSigner: false },
+                { pubkey: new anchor.web3.PublicKey('Ha2s5qVfW8HFpTwkEfuJm6NLJSSoBgLSKTTkMaCoVKpS'), isWritable: false, isSigner: false },
+                { pubkey: new anchor.web3.PublicKey('AC5aWi4GXaCMfXPKTfASPCEuvNpQs3KgccTYqywqR8HM'), isWritable: false, isSigner: false },
             ],
         }
-    );
+    );*/
+/*    const tx = await program.rpc.run('(cmd "token_transfer" 0 1 2 3 (+ 7 4) 4)',
+        {
+            remainingAccounts: [
+                { pubkey: new anchor.web3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'), isWritable: false, isSigner: false },
+                { pubkey: new anchor.web3.PublicKey('Ha2s5qVfW8HFpTwkEfuJm6NLJSSoBgLSKTTkMaCoVKpS'), isWritable: true, isSigner: false },
+                { pubkey: new anchor.web3.PublicKey('AC5aWi4GXaCMfXPKTfASPCEuvNpQs3KgccTYqywqR8HM'), isWritable: true, isSigner: false },
+                { pubkey: provider.wallet.publicKey, isWritable: false, isSigner: true },
+            ],
+        }
+    );*/
     console.log("Your transaction signature", tx);
   });
 });
